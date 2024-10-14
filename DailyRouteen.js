0b787c0e850151eb5651ede0a6dee04af9c518a4
Shivam Thakre <st548501@gmail.com>
2024-10-14 22:53:35 +0530

    Dailywork

---
+++ DailyRouteen.js
@@ -0,0 +1,4 @@
+let Dailywork  = ["Freshup","Lunch","Masai Classes","Evening Walk","Practice"]
+Dailywork.unshift("Wokeup");
+Dailywork.pop();
+console.log(Dailywork);
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # secondRepo
+# Heading1
+## Heading 2
+### Heading 3
+
+**Bold**
+***Bold italic***
+-- list1 --
+-- list2 --
