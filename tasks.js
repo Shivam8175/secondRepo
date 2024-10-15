c31f4827aedcaf695b82c7e5a3b95beb6964a0a1
Shivam Thakre <st548501@gmail.com>
2024-10-15 22:50:13 +0530

    Rewrite code

--- tasks.js
+++ tasks.js
@@ -1,5 +1,16 @@
 let tasks = ["wokeup","freshup","lunch","evening Walk","Dinner"];
-tasks.shift();
-tasks.unshift("Breakfast","study");
-tasks[5] = "Sleep";
+let n = tasks.length;
+for(let i=0;i<n-1;i++){
+    tasks[i] = tasks[i+1];
+}
+n--;
+let hightasks1 = "Revision";
+let hightasks2 = "Sleep";
+n+=2;
+for(let i=n-1;i>=2;i--){
+    tasks[i] = tasks[i-2];
+}
+tasks[0] = hightasks1
+tasks[1] = hightasks2
+tasks[n-1] =  "rest";
 console.log(tasks);
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,6 @@
+let hightasks1 = "Revision";
+// let hightasks2 = "Sleep";
+// for(let i=n;i>=2;i--){
+//     tasks[i] = tasks[i-2];
+// }
+// tasks[n] =  "rest";
